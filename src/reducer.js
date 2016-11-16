647093e5be06c2f03d10fc2b40291f226778af16
dong.keke <dong.keke@tongbanjie.com>
2016-11-16 11:21:17 +0800

    完成投票服务的服务器端开发

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,5 @@
 import {setEntries, next, vote, INITIAL_STATE} from './core';
-
+//一个应用有大量的action，如何解决
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
   case 'SET_ENTRIES':
@@ -8,7 +8,7 @@ export default function reducer(state = INITIAL_STATE, action) {
     return next(state);
   case 'VOTE':
     return state.update('vote',
-                        voteState => vote(voteState, action.entry));
+                        voteState => vote(voteState, action.entry));//提供给业务逻辑它仅需要的数据的解决方案
   }
   return state;
 }
--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ import Server from 'socket.io';
 export function startServer(store) {
   const io = new Server().attach(8090);
 
+  //当store的状态发生变化时，发送当前的状态给所有处于连接的客户端。
   store.subscribe(
     () => io.emit('state', store.getState().toJS())
   );
 
+  //每当新客户端连接服务器端时,发送当前的状态给该客户端。
   io.on('connection', (socket) => {
     socket.emit('state', store.getState().toJS());
     socket.on('action', store.dispatch.bind(store));
